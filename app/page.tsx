db4880620aa8becd83ab74ca9560b9eea51ae6b7
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2025-02-04 00:55:11 +0600

    dark for all

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 import RecentProjects from '@/components/RecentProjects'
+import { main } from 'framer-motion/client'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <main className="dark bg-black" >
+    <div className="dark">
 
       <Hero />
       <RecentProjects />
       <Footer />
     </div>
+    </main>
   )
 }
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,6 +6,7 @@ import { HeroHighlight, Highlight } from "./ui/hero-highlight";
 
 const Hero = () => {
     return (
+        <div>
         <HeroHighlight>
           <motion.h1
             initial={{
@@ -26,8 +27,19 @@ const Hero = () => {
             <Highlight className="text-black dark:text-white">
               Musaddiq Rafi 
             </Highlight>
+
           </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: [20, -5, 0] }}
+            transition={{ duration: 2, ease: [0.4, 0.0, 0.2, 1] }}
+            className="text-lg px-4 md:text-xl lg:text-2xl text-neutral-700 dark:text-white max-w-2xl leading-relaxed lg:leading-snug text-center mx-auto mt-4"
+          >
+            I&apos;m an aspiring software Engineer from Bangladesh 🇧🇩
+          </motion.p>
         </HeroHighlight>
+        
+        </div>
       );
     }
 
