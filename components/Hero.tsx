baf1053e72b4adbd11b30c331d0ae0793c3f71a2
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2025-02-04 00:27:57 +0600

    added header

---
+++ components/Hero.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from 'react';
+import { motion } from "framer-motion";
+import { HeroHighlight, Highlight } from "./ui/hero-highlight";
+
+
+const Hero = () => {
+    return (
+        <HeroHighlight>
+          <motion.h1
+            initial={{
+              opacity: 0,
+              y: 20,
+            }}
+            animate={{
+              opacity: 1,
+              y: [20, -5, 0],
+            }}
+            transition={{
+              duration: 0.5,
+              ease: [0.4, 0.0, 0.2, 1],
+            }}
+            className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+          >
+            Hi ! Welcome to my digital garden! This is {" "}
+            <Highlight className="text-black dark:text-white">
+              Musaddiq Rafi 
+            </Highlight>
+          </motion.h1>
+        </HeroHighlight>
+      );
+    }
+
+export default Hero
