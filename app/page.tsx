0175cd40225a64cc39c6d6b7b4f9a28930fe834a
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2025-02-04 22:42:58 +0600

    builded bento gird part

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 import RecentProjects from '@/components/RecentProjects'
+import InfoGrid from '@/components/InfoGrid'
 import React from 'react'
 
 const page = () => {
   return (
-    <main className="dark bg-black" >
+    <main className="dark bg-black font-extrabold" >
     <div className="dark">
 
       <Hero />
+      <InfoGrid />
       <RecentProjects />
       <Footer />
     </div>
