5b71a8f282eb400bfe5e30ffc1ba56f05e0f8d48
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2025-02-04 23:49:26 +0600

    added floating bar and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@ import Hero from '@/components/Hero'
 import RecentProjects from '@/components/RecentProjects'
 import InfoGrid from '@/components/InfoGrid'
 import React from 'react'
+import { Dock } from '@/components/Dock'
 
 const page = () => {
   return (
     <main className="dark bg-black font-extrabold" >
     <div className="dark">
-
+      <Dock />
       <Hero />
       <InfoGrid />
       <RecentProjects />
