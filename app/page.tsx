f864d4a794203dfae3540c56f5231b55fd5e49e8
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2025-02-05 01:27:14 +0600

    added new files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
-import RecentProjects from '@/components/RecentProjects'
+import { RecentProjects } from '@/components/RecentProjects'
 import InfoGrid from '@/components/InfoGrid'
 import React from 'react'
 import { Dock } from '@/components/Dock'
@@ -9,6 +9,8 @@ const page = () => {
   return (
     <main className="dark bg-black font-extrabold" >
     <div className="dark">
+
+      {/* <p className='text-4xl text-red-600'> THE WEBSITE IS UNER CONSTRUCTION PLEASE VISIT LATER</p> */}
       <Dock />
       <Hero />
       <InfoGrid />
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,9 +1,316 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React, { useEffect, useId, useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
-const RecentProjects = () => {
+export function RecentProjects() {
+  const [active, setActive] = useState<(typeof cards)[number] | boolean | null>(
+    null
+  );
+  const id = useId();
+  const ref = useRef<HTMLDivElement>(null!);
+
+  useEffect(() => {
+    function onKeyDown(event: KeyboardEvent) {
+      if (event.key === "Escape") {
+        setActive(false);
+      }
+    }
+
+    if (active && typeof active === "object") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [active]);
+
+  useOutsideClick(ref, () => setActive(null));
+
   return (
-    <div>RecentProjects</div>
-  )
+    <>
+    <h1 className="text-3xl md:text-5xl text-white py-10 px-4 md:px-60 font-bold text-center mt-10">
+      Some of my Recent <span className="text-purple-300">Projects</span>
+    </h1>
+      <AnimatePresence>
+        {active && typeof active === "object" && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 bg-black/20 h-full w-full z-10"
+          />
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {active && typeof active === "object" ? (
+          <div className="fixed inset-0  grid place-items-center z-[100]">
+            <motion.button
+              key={`button-${active.title}-${id}`}
+              layout
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+              }}
+              exit={{
+                opacity: 0,
+                transition: {
+                  duration: 0.05,
+                },
+              }}
+              className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
+              onClick={() => setActive(null)}
+            >
+              <CloseIcon />
+            </motion.button>
+            <motion.div
+              layoutId={`card-${active.title}-${id}`}
+              ref={ref}
+              className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+            >
+              <motion.div layoutId={`image-${active.title}-${id}`}>
+                <Image
+                  priority={true}
+                  width={800}
+                  height={800}
+                  src={active.src}
+                  alt={active.title}
+                  className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
+                  
+                />
+              </motion.div>
+
+              <div>
+                <div className="flex justify-between items-start p-4">
+                  <div className="">
+                    <motion.h3
+                      layoutId={`title-${active.title}-${id}`}
+                      className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
+                    >
+                      {active.title}
+                    </motion.h3>
+                    <motion.p
+                      layoutId={`description-${active.description}-${id}`}
+                      className="text-neutral-600 dark:text-neutral-400 text-base"
+                    >
+                      {active.description}
+                    </motion.p>
+                  </div>
+
+                  <motion.a
+                    layout
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    href={active.ctaLink}
+                    target="_blank"
+                    className="px-4 py-3 text-sm rounded-full font-bold bg-purple-600 text-white"
+                  >
+                    {active.ctaText}
+                  </motion.a>
+                </div>
+                <div className="pt-4 relative px-4">
+                  <motion.div
+                    layout
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
+                  >
+                    {typeof active.content === "function"
+                      ? active.content()
+                      : active.content}
+                  </motion.div>
+                </div>
+              </div>
+            </motion.div>
+          </div>
+        ) : null}
+      </AnimatePresence>
+      <ul className="max-w-2xl mx-auto w-full grid grid-cols-1 md:grid-cols-2 items-start gap-4">
+        {cards.map((card, index) => (
+          <motion.div
+            layoutId={`card-${card.title}-${id}`}
+            key={card.title}
+            onClick={() => setActive(card)}
+            className="p-4 flex flex-col  hover:bg-neutral-50 dark:hover:bg-neutral-900 rounded-xl cursor-pointer"
+          >
+            <div className="flex gap-4 flex-col  w-full">
+              <motion.div layoutId={`image-${card.title}-${id}`}>
+                <Image
+                  width={800}
+                  height={800}
+                  src={card.src}
+                  alt={card.title}
+                  className="h-60 w-full  rounded-lg object-cover object-top"
+                />
+              </motion.div>
+              <div className="flex justify-center items-center flex-col">
+                <motion.h3
+                  layoutId={`title-${card.title}-${id}`}
+                  className="font-medium text-neutral-800 dark:text-neutral-200 text-center md:text-left text-base"
+                >
+                  {card.title}
+                </motion.h3>
+                <motion.p
+                  layoutId={`description-${card.description}-${id}`}
+                  className="text-neutral-600 dark:text-neutral-400 text-center md:text-left text-base"
+                >
+                  {card.description}
+                </motion.p>
+              </div>
+            </div>
+          </motion.div>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default RecentProjects
+export const CloseIcon = () => {
+  return (
+    <motion.svg
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      exit={{
+        opacity: 0,
+        transition: {
+          duration: 0.05,
+        },
+      }}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="h-4 w-4 text-black"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M18 6l-12 12" />
+      <path d="M6 6l12 12" />
+    </motion.svg>
+  );
+};
+
+const cards = [
+  {
+    
+    title: "This Portfolio Website",
+    description: " Next.js, Framer Motion, TailwindCSS, TypeScript, Aceterity UI",
+    src: "/assets/Portfolio.png",
+    ctaText: "Github",
+    ctaLink: "https://github.com/musaddiq-rafi/portfolio_ok",
+    content: () => {
+      return (
+        <p>
+          This is a portfolio website built using Next.js, Framer Motion,
+          TailwindCSS, TypeScript, and Aceterity UI. It showcases my recent
+          projects, skills, and experience in web development.
+          Floating dock, animations are some of the features of this website.
+           <br /> <br /> The
+          website features a responsive design, smooth animations, and a clean
+          layout that highlights my work and achievements. With a focus on
+          usability and accessibility, it provides an engaging user experience
+          that is both informative and visually appealing.
+        </p>
+      );
+    },
+  },
+  {
+    title: "Nike Landing Page",
+    description: "React, TailwindCSS",
+    src: "/assets/Nike.png",
+    ctaText: "Github",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <p>
+         This is a Nike landing page built using React and TailwindCSS. 
+         It features a clean and modern design that showcases the latest Nike products and collections.
+         No external UI libraries were used in this project. <br /> <br /> The landing page includes a hero section, product grid, and footer, all styled with TailwindCSS utility classes.
+
+        </p>
+      );
+    },
+  },
+
+  {
+    
+    title: "YC Directory- Full Stack (WIP)",
+    description: "Next.js, TailwindCSS , Sanity",
+    src: "/assets/YCDirectory.png",
+    ctaText: "Github",
+    ctaLink: "https://github.com/musaddiq-rafi/startup-directory",
+    content: () => {
+      return (
+        <p>
+          YC Directory is a full stack web app built using Next.js, TailwindCSS and Sanity. 
+          This project is from JSMastery youtube channel and it is still in progress. <br /> <br /> The app will allow users to browse and search through a directory of Y Combinator startups, view details about each startup, and filter startups based on various criteria.
+        </p>
+      );
+    },
+  },
+  {
+    
+    title: "Word Waffle Game",
+    description: "Java, Java Swing",
+    src: "/assets/WordWaffle.png",
+    ctaText: "Github",
+    ctaLink: "https://github.com/SillyCatto/word_waffle",
+    content: () => {
+      return (
+        <p>
+          Word Waffle is a word game built using Java and Java Swing that helps users to learn English in a fun and interactive way.
+          It was made for OOP2 course along with my Teammate named <span className=" text-blue-500">Raiyan Muhtasim</span>.
+          
+        </p>
+      );
+    },
+  },
+  {
+    
+    title: "SigmaGPT",
+    description: "HTML, CSS , React and Gemini API by Google",
+    src: "/assets/SigmaGPT.jpeg",
+    ctaText: "Github",
+    ctaLink: "https://github.com/musaddiq-rafi/SigmaGPT",
+    content: () => {
+      return (
+        <p>
+          It was a funny clone of ChatGPT that talks in GenZ languages.
+          It was used by a lot of people and got a lot of attention on Facebook. It was a fun project to work on.
+          
+        </p>
+      );
+    },
+  },
+  {
+    
+    title: "HabitUP - Chrome Extension",
+    description: "Html, CSS, JS",
+    src: "/assets/HabitUp.jpeg",
+    ctaText: "Github",
+    ctaLink: "https://github.com/musaddiq-rafi/HabitUp",
+    content: () => {
+      return (
+        <p>
+          It was a rapid project that was made within 5-6 hours using AI. It helps user to keep track on their habit and shows meme. The meme changes based on the days and also habit completion.
+          It is still used by some people.
+        </p>
+      );
+    },
+  },
+];
